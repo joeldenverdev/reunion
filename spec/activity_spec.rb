ea67e52506df04033fe0c3af5391eaa6fb8083b9
Joel Grant <joelmgrant@me.com>
2021-11-05 21:47:11 -0600

    Test: Add activity tests

---
+++ spec/activity_spec.rb
@@ -0,0 +1,38 @@
+require './lib/activity'
+
+describe Activity do
+  before(:each) do
+    @activity = Activity.new("Brunch")
+  end
+
+  describe '#initialize' do
+    it 'returns an instance of Activity' do
+      expect(@activity).to be_a Activity
+    end
+
+    it 'stores and returns the name' do
+      expect(@activity.name).to eq("Brunch")
+    end
+
+    it 'stores and returns a hash of participants' do
+      expect(@activity.participants).to eq({})
+    end
+  end
+
+  describe '#add_participant' do
+    it 'adds new participants to the list' do
+      @activity.add_participant("Maria", 20)
+      expect(@activity.participants).to eq({"Maria" => 20})
+    end
+  end
+
+  describe '#total_cost' do
+    it 'returns the total cost of an activity' do
+      @activity.add_participant("Maria", 20)
+      @activity.add_participant("Luther", 40)
+      expect(@activity.total_cost).to eq(60)
+    end
+  end
+
+
+end
