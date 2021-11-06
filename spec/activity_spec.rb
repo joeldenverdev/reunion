77eaa1e66ff8ccdb4eabfec4b1be4dc63ba4cb8f
Joel Grant <joelmgrant@me.com>
2021-11-05 22:33:29 -0600

    Test: #add_activity test

--- spec/activity_spec.rb
+++ spec/activity_spec.rb
@@ -42,15 +42,16 @@ describe Activity do
       expect(@activity.split).to eq(30)
     end
   end
-  
+
   describe '#owed' do
     it 'calculates how much is owed after the split' do
       @activity.add_participant("Maria", 20)
       @activity.add_participant("Luther", 40)
       @activity.split
-      expect(@activity.owed).to eq({"Maria" => 10, "Luther" => -10})
+      expect(@activity.owed).to eq({"Maria" => -10, "Luther" => 10})
     end
   end
+
 
 
 end
