7e68733b40e8808501dcf1a1ea8fa54296f8735d
Joel Grant <joelmgrant@me.com>
2021-11-06 23:17:57 -0600

    Feat: Add #breakout

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -1,5 +1,5 @@
 class Reunion
-  attr_reader :name
+  attr_reader :name, :activities
 
   def initialize(name)
     @name = name
@@ -9,4 +9,27 @@ class Reunion
   def add_activity(activity)
     @activities << activity
   end
+
+  def total_cost
+    cost = @activities.map { |activity| activity.participants.values.sum }.sum
+  end
+
+  def breakout
+    total_money = Hash.new(0)
+    #
+    # participants = @activities.map do |activity|
+    #   activity.participants.transform_values do |participant|
+    #     participant - activity.split
+    #   end
+    # end
+
+    @activities.each do |activity|
+      activity.owed.each do |name, owed|
+        total_money[name] -= owed
+      end
+    end
+
+    return total_money
+  end
+
 end
